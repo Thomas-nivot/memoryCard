214f0cd7bafd4a29ae1ca70331ac2d2e33ce6e0a
Thomas Nivot <81165239+Thomas-nivot@users.noreply.github.com>
2021-04-16 11:51:24 +0200

    Add files via upload

---
+++ Jeu_des_paires_nivot_thomas/js/script.js
@@ -0,0 +1,91 @@
+const cards = document.querySelectorAll('.game-card');
+const victoryScreen = document.getElementById('victory');
+const deafeatScreen = document.getElementById('game-over-text');
+const countFlip = document.getElementById('flip-count');
+const countVictory = document.getElementById('victory-count');
+// VARIABLES
+let hasFlippedCard = false;
+// LOCKER
+let lockBoard = false;
+let firstCard, secondCard;
+let totalFlip = 0;
+let pointVictory = 0;
+
+
+/*TIMER*/
+var timeleft = 50;
+var downloadTimer = setInterval(function(){
+    if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    gameOver();
+    } else {
+    document.getElementById("time-remaining").innerHTML = timeleft + " seconds remaining";
+    }
+    timeleft -= 1;
+}, 1000);
+
+function gameOver(){
+    deafeatScreen.classList.add('display');
+}
+
+
+
+// FUNCTION FLIP CARD
+function flipCard() {
+    // LOCKER DE JEU
+    if(lockBoard) return;
+    // DOUBLE CLICK
+    if(this === firstCard) return;
+    // AJOUT DE LA CLASS FLIP
+    this.classList.toggle('flip');
+    if (!hasFlippedCard) {
+        // PREMIER CLICK
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+        // SECOND CLICK
+        secondCard = this;
+        checkForMatch();
+}
+function checkForMatch() {
+    if (firstCard.dataset.cardname === secondCard.dataset.cardname) {
+        disableCards();
+    } else {
+        unflipCards();
+    }
+}
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+    totalFlip++;
+    pointVictory++;
+    countFlip.innerText = totalFlip;
+    countVictory.innerText = pointVictory;
+    if (pointVictory === 6) {
+        victoryScreen.classList.add('display');
+    }
+}
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        totalFlip++;
+        countFlip.innerText = totalFlip;
+        resetBoard();
+    }, 1000);
+}
+// RESET LES VALEURS
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+cards.forEach(card => card.addEventListener('click', flipCard));
